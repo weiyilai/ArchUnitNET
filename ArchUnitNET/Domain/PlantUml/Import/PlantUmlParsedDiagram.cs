fd935ad79e3339d746477bd97a012ceb17a74a05
Malte Kunath <malte.kunath@tngtech.com>
2026-01-09 19:23:13 +0100

    Enable strong-name signing for all projects and tests #223
    Signed-off-by: Malte Kunath <malte.kunath@tngtech.com>

--- ArchUnitNET/Domain/PlantUml/Import/PlantUmlParsedDiagram.cs
+++ ArchUnitNET/Domain/PlantUml/Import/PlantUmlParsedDiagram.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Runtime.CompilerServices;
 
-[assembly: InternalsVisibleTo("ArchUnitNETTests")]
+[assembly: InternalsVisibleTo(
+    "ArchUnitNETTests, PublicKey=002400000480000094000000060200000024000052534131000400000100010061b41c9ec5575b4b70ba7feca6e64a865c64f15dfc76a057f7fb3b5a0bbecab64b0ad0ec04ea20fa0d370357c786bd80d1bf92ac91ef021ccd9394e08871eca28f9ad8ae58f18bb518707839f38a8e3a10f33e0a0525f3e4bae3e4da391efe7d0fb41cc03f63923065aa1efe6f440bd0669284668863f87284a8a40d3b604db9"
+)]
 
 namespace ArchUnitNET.Domain.PlantUml.Import
 {
